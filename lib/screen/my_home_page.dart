ad28def3bda50c8c525f1ffc4f722efbd55e5530
supakit-code <it_harem@rmutl.ac.th.>
2024-05-16 17:13:26 +0700

    finish

--- lib/screen/my_home_page.dart
+++ lib/screen/my_home_page.dart
@@ -8,8 +8,43 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  @override
+  int _counter = 0;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      if (_counter < 1) {
+        _counter = 0;
+      } else {
+        _counter--;
+      }
+    });
+  }
+
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My increment counter"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(" count $_counter time"),
+            ElevatedButton(
+              onPressed: _incrementCounter,
+              child: const Icon(Icons.add),
+            ),
+            ElevatedButton(
+              onPressed: _decrementCounter,
+              child: const Icon(Icons.remove),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
